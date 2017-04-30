3ce66b0cbd261e5cd8bde163ad8330ebc5208482
anahenneberke <ana.henneberke@gmail.com>
2017-04-30 16:19:22 +0100

    Updated absolute path of dist due to npm breaking change on new version

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: '/dist',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
